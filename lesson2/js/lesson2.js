56c207d2bfcdb8ce3c3361b58ee80a562883f6e6
jah83440 <har18043@byui.edu>
2022-09-24 23:08:13 -0600

    Cleaning up Javascript

--- lesson2/js/lesson2.js
+++ lesson2/js/lesson2.js
@@ -1,6 +1,4 @@
 const date = new Date();
 document.querySelector("footer div span").textContent = date.getFullYear();
 const lastmodified = new Date(document.lastModified);
-document.getElementById("lastupdatedate2").textContent = `${
-  lastmodified.getMonth() + 1
-}/${lastmodified.getDate()}/${lastmodified.getFullYear()} ${lastmodified.getHours()}:${lastmodified.getMinutes()}:${lastmodified.getSeconds()}`;
+document.getElementById("lastupdatedate2").textContent = `${lastmodified.getMonth() + 1}/${lastmodified.getDate()}/${lastmodified.getFullYear()} ${lastmodified.getHours()}:${lastmodified.getMinutes()}:${lastmodified.getSeconds()}`;
