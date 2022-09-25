049f1105f10bee33d136cf4e6b49b955f9c0d62a
jah83440 <har18043@byui.edu>
2022-09-24 23:03:09 -0600

    Design Principles Part 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const date = new Date();
 document.querySelector("footer div span").textContent = date.getFullYear();
 const lastmodified = new Date(document.lastModified);
-document.getElementById("lastupdatedate").textContent = `${lastmodified.getMonth()+1}/${lastmodified.getDate()}/${lastmodified.getFullYear()}`;
+document.getElementById("lastupdatedate").textContent = `${
+    lastmodified.getMonth() + 1
+  }/${lastmodified.getDate()}/${lastmodified.getFullYear()} ${lastmodified.getHours()}:${lastmodified.getMinutes()}:${lastmodified.getSeconds()}`;
+  
--- lesson2/js/lesson2.js
+++ lesson2/js/lesson2.js
@@ -1,4 +1,6 @@
 const date = new Date();
 document.querySelector("footer div span").textContent = date.getFullYear();
 const lastmodified = new Date(document.lastModified);
-document.getElementById("lastupdatedate2").textContent = `${lastmodified.getMonth()+1}/${lastmodified.getDate()}/${lastmodified.getFullYear()}`;
+document.getElementById("lastupdatedate2").textContent = `${
+  lastmodified.getMonth() + 1
+}/${lastmodified.getDate()}/${lastmodified.getFullYear()} ${lastmodified.getHours()}:${lastmodified.getMinutes()}:${lastmodified.getSeconds()}`;
