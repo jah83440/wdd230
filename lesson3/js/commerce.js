d7587d67d6e9e02be47c5c7632dfb766f04df1d1
jah83440 <har18043@byui.edu>
2022-10-04 17:54:52 -0600

    Lesson 03 Catch-up

---
+++ lesson3/js/commerce.js
@@ -0,0 +1,4 @@
+const date = new Date();
+document.querySelector("footer div span").textContent = date.getFullYear();
+const lastmodified = new Date(document.lastModified);
+document.getElementById("lastupdatedate").textContent = `${lastmodified.getMonth() + 1}/${lastmodified.getDate()}/${lastmodified.getFullYear()} ${lastmodified.getHours()}:${lastmodified.getMinutes()}:${lastmodified.getSeconds()}`;
